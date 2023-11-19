dceb50e5c3ca8d4d1067ca594c22caf9db0b7881
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2023-11-20 00:19:37 +0530

    Create vendor/pi/page.js

---
+++ frontend/app/vendor/pi/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../../components/Container";
+import FormWrap from "../../components/FormWrap";
+import FormPI from "./piForm";
+
+export default function Register() {
+  return (
+    <Container>
+      <FormWrap>
+        <FormPI />
+      </FormWrap>
+    </Container>
+  );
+}
