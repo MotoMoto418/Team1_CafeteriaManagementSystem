6a9df167083952734272570e4a048d0471ea0cb5
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2023-11-20 00:21:16 +0530

    Create vendor/si/page.js

---
+++ frontend/app/vendor/si/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../../components/Container";
+import FormWrap from "../../components/FormWrap";
+import FormSI from "./siForm";
+
+export default function Register() {
+  return (
+    <Container>
+      <FormWrap>
+        <FormSI />
+      </FormWrap>
+    </Container>
+  );
+}
